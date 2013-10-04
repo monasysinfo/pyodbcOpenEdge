17f4066178b52ef4654b4fa25cb8da273bc0232d
J-Y Priou <jy.priou@free.fr>
2013-10-04 12:53:35 +0200

    Ajout des tests de validation

---
+++ tests/local_settings.py
@@ -0,0 +1,9 @@
+DATABASE_ENGINE = 'OpenEdge.pyodbc'
+
+# Define these settings according to your Openedge Server setup:
+
+DATABASE_NAME = 'eslemien'
+DATABASE_USER = 'SGS'
+DATABASE_PASSWORD = ''
+DATABASE_HOST = 'localhost' # The dataserver definition from freetds.conf
+DATABASE_PORT = '50000'
