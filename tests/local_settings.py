3c68a0c70cc67193ae1210912d852a9bf70297e5
J-Y Priou <jy.priou@free.fr>
2013-10-11 18:56:30 +0200

    Delete tests

--- tests/local_settings.py
+++
@@ -1,9 +0,0 @@
-DATABASE_ENGINE = 'OpenEdge.pyodbc'
-
-# Define these settings according to your Openedge Server setup:
-
-DATABASE_NAME = 'eslemien'
-DATABASE_USER = 'SGS'
-DATABASE_PASSWORD = ''
-DATABASE_HOST = 'localhost' # The dataserver definition from freetds.conf
-DATABASE_PORT = '50000'
