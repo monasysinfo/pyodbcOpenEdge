bd93315654bb89c90865e1ec928c9f85f2810447
J-Y Priou <jy.priou@free.fr>
2013-12-12 11:45:29 +0100

    Add new tags

--- setup.py
+++ setup.py
@@ -15,25 +15,25 @@ setup(
     version='1.0',
     description='A Django Backend for OpenEdge databases (Progress)',
     long_description='''
-This module allows the use of the OpenEdge (Progress) databases through Django. It provides the ability to create tables from Django models, or to access existing tables (4GL or SQL) in your Django application.
+This module allows the use of the OpenEdge (Progress) databases through Django. It uses pyodbc for the db connexions. It provides the ability to create tables from Django models, or to access existing tables (4GL or SQL) in your Django application.
 The South integration permits to migrate the database schema.
 ''',
     author='Jean-Yves Priou',
-    author_email='jy.priou@free.fr',
+    author_email='monasysinfo@gmail.com',
     platforms="Independent",
     url='http://www.jypriou.fr/',
     packages=['OpenEdge','OpenEdge.OEmodels','OpenEdge.pyodbc','OpenEdge.south'],
-    license="MIT",
+    license="BSD",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: DataBases :: Backends :: Django',
+        'Topic :: DataBases :: Backends :: Django :: OpenEdge :: odbc :: pyodbc :: Progress',
         'Topic :: Software Development :: Libraries',
         'Topic :: Utilities'
     ],
-    keywords='django openedge progress',
+    keywords='django openedge progress odbc pyodbc',
 )
